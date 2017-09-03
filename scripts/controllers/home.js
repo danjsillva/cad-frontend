50ce7d3ab0a04120841d20d265ba68e5d1673658
Daniel Silva <danjsillva@gmail.com>
2017-09-02 22:29:22 -0300

    adicionado cards e outras informacoes dashboard

--- scripts/controllers/home.js
+++ scripts/controllers/home.js
@@ -3,6 +3,54 @@
 */
 angular
 .module('cadweb')
-.controller('HomeCtrl', function ($scope) {
+.controller('HomeCtrl', function (api, $scope, $rootScope, $http, toaster, notification) {
+    $scope.discentes = [];
+    $scope.docentes = [];
 
+    $scope.getProjetos = function () {
+        var url = api.baseUrl + '/projetos';
+
+        $http.get(url).then(
+            function (response) {
+                $scope.projetos = response.data;
+            },
+            function (response) {
+                notification.show(response.status);
+            }
+        );
+    };
+
+    $scope.getDiscentes = function () {
+        var url = api.baseUrl + '/discentes';
+
+        $http.get(url).then(
+            function (response) {
+                $scope.discentes = response.data;
+            },
+            function (response) {
+                notification.show(response.status);
+            }
+        );
+    };
+
+    // lista todos os registros
+    $scope.getDocentes = function () {
+        var url = api.baseUrl + '/docentes';
+
+        $http.get(url).then(
+            function (response) {
+                $scope.docentes = response.data;
+            },
+            function (response) {
+                notification.show(response.status);
+            }
+        );
+    };
+
+    if ($rootScope.isAuthenticated) {
+        $scope.getProjetos();
+        $scope.getDiscentes();
+        $scope.getDocentes();
+
+    }
 });
